69f9fec73a0f2d63ea2f26cf37442965ca5e4961
Tads <tmuzondo4@gmail.com>
2020-09-15 16:39:58 +0200

    functions skeleton code uploaded for fetching from pi module

--- initial_api/initial_api.py
+++ initial_api/initial_api.py
@@ -1,1 +1,23 @@
 """Main module."""
+def getliveStream():
+    """
+    This function would get 
+    the live strean video 
+    from our pi
+    """
+    
+def parkOccupied():
+    """
+    This function would get 
+    get the boolean value 
+    from our pi of whether
+    a parking space is filled or not
+    
+    """ 
+def getLocation():
+    """"
+    gets the exact location
+    of where our raspberry 
+    pi situated
+    """
+    
